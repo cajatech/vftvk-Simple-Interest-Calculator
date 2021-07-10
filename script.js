8e191863ca798e5532b9d34998f16a6960191bec
iThink <caja.tech@gmail.com>
2021-07-09 19:36:08 -0500

    update project

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = parseFloat(document.getElementById("principal").value);
+    var rate = parseFloat(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * rate / 100;
+    var yearInTheFuture = new Date().getFullYear() + years;
+    document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,<br/>" + 
+    "at an interest rate of <mark>" + rate + "</mark>.<br/>" +
+    "You will receive an amount of  <mark>" + interest + "</mark>,<br/> " + 
+    "in the year <mark>" + yearInTheFuture + "</mark>";
     
 }
+function getSliderValue(){
+    document.getElementById("rate_val").innerHTML = document.getElementById("rate").value;
+}
+
         
